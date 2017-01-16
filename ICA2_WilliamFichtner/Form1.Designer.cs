125ee838737431e73db6da217c182767d99d488a
William Fichtner <wfichtner1@nait.ca>
2017-01-16 15:38:49 -0700

    Done

--- ICA2_WilliamFichtner/Form1.Designer.cs
+++ ICA2_WilliamFichtner/Form1.Designer.cs
@@ -30,15 +30,15 @@
         {
             this.UI_CostLabel = new System.Windows.Forms.Label();
             this.UI_NumberLabel = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.UI_CostBox = new System.Windows.Forms.TextBox();
+            this.UI_PurchaseBox = new System.Windows.Forms.TextBox();
             this.UI_SubtotalLabel = new System.Windows.Forms.Label();
             this.UI_GSTLabel = new System.Windows.Forms.Label();
             this.UI_TotalLabel = new System.Windows.Forms.Label();
             this.UI_CalculateButton = new System.Windows.Forms.Button();
-            this.UI_SubtotalAmount = new System.Windows.Forms.Label();
-            this.UI_GSTAmount = new System.Windows.Forms.Label();
-            this.UI_TotalAmount = new System.Windows.Forms.Label();
+            this.UI_SubtotalAmountLabel = new System.Windows.Forms.Label();
+            this.UI_GSTAmountLabel = new System.Windows.Forms.Label();
+            this.UI_TotalAmountLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // UI_CostLabel
@@ -60,19 +60,21 @@
             this.UI_NumberLabel.TabIndex = 10;
             this.UI_NumberLabel.Text = "Number to Purchase:";
             // 
-            // textBox1
+            // UI_CostBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(200, 44);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.UI_CostBox.Location = new System.Drawing.Point(200, 44);
+            this.UI_CostBox.Name = "UI_CostBox";
+            this.UI_CostBox.Size = new System.Drawing.Size(100, 20);
+            this.UI_CostBox.TabIndex = 0;
+            this.UI_CostBox.TextChanged += new System.EventHandler(this.UI_CostBox_TextChanged);
             // 
-            // textBox2
+            // UI_PurchaseBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(200, 78);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this.UI_PurchaseBox.Location = new System.Drawing.Point(200, 78);
+            this.UI_PurchaseBox.Name = "UI_PurchaseBox";
+            this.UI_PurchaseBox.Size = new System.Drawing.Size(100, 20);
+            this.UI_PurchaseBox.TabIndex = 1;
+            this.UI_PurchaseBox.TextChanged += new System.EventHandler(this.UI_PurchaseBox_TextChanged);
             // 
             // UI_SubtotalLabel
             // 
@@ -104,54 +106,56 @@
             // 
             // UI_CalculateButton
             // 
+            this.UI_CalculateButton.Enabled = false;
             this.UI_CalculateButton.Location = new System.Drawing.Point(157, 279);
             this.UI_CalculateButton.Name = "UI_CalculateButton";
             this.UI_CalculateButton.Size = new System.Drawing.Size(75, 23);
             this.UI_CalculateButton.TabIndex = 2;
             this.UI_CalculateButton.Text = "Calculate";
             this.UI_CalculateButton.UseVisualStyleBackColor = true;
+            this.UI_CalculateButton.Click += new System.EventHandler(this.UI_CalculateButton_Click);
             // 
-            // UI_SubtotalAmount
+            // UI_SubtotalAmountLabel
             // 
-            this.UI_SubtotalAmount.AutoSize = true;
-            this.UI_SubtotalAmount.Location = new System.Drawing.Point(197, 151);
-            this.UI_SubtotalAmount.Name = "UI_SubtotalAmount";
-            this.UI_SubtotalAmount.Size = new System.Drawing.Size(34, 13);
-            this.UI_SubtotalAmount.TabIndex = 4;
-            this.UI_SubtotalAmount.Text = "$0.00";
+            this.UI_SubtotalAmountLabel.AutoSize = true;
+            this.UI_SubtotalAmountLabel.Location = new System.Drawing.Point(197, 151);
+            this.UI_SubtotalAmountLabel.Name = "UI_SubtotalAmountLabel";
+            this.UI_SubtotalAmountLabel.Size = new System.Drawing.Size(34, 13);
+            this.UI_SubtotalAmountLabel.TabIndex = 4;
+            this.UI_SubtotalAmountLabel.Text = "$0.00";
             // 
-            // UI_GSTAmount
+            // UI_GSTAmountLabel
             // 
-            this.UI_GSTAmount.AutoSize = true;
-            this.UI_GSTAmount.Location = new System.Drawing.Point(197, 190);
-            this.UI_GSTAmount.Name = "UI_GSTAmount";
-            this.UI_GSTAmount.Size = new System.Drawing.Size(34, 13);
-            this.UI_GSTAmount.TabIndex = 6;
-            this.UI_GSTAmount.Text = "$0.00";
+            this.UI_GSTAmountLabel.AutoSize = true;
+            this.UI_GSTAmountLabel.Location = new System.Drawing.Point(197, 190);
+            this.UI_GSTAmountLabel.Name = "UI_GSTAmountLabel";
+            this.UI_GSTAmountLabel.Size = new System.Drawing.Size(34, 13);
+            this.UI_GSTAmountLabel.TabIndex = 6;
+            this.UI_GSTAmountLabel.Text = "$0.00";
             // 
-            // UI_TotalAmount
+            // UI_TotalAmountLabel
             // 
-            this.UI_TotalAmount.AutoSize = true;
-            this.UI_TotalAmount.Location = new System.Drawing.Point(197, 235);
-            this.UI_TotalAmount.Name = "UI_TotalAmount";
-            this.UI_TotalAmount.Size = new System.Drawing.Size(34, 13);
-            this.UI_TotalAmount.TabIndex = 8;
-            this.UI_TotalAmount.Text = "$0.00";
+            this.UI_TotalAmountLabel.AutoSize = true;
+            this.UI_TotalAmountLabel.Location = new System.Drawing.Point(197, 235);
+            this.UI_TotalAmountLabel.Name = "UI_TotalAmountLabel";
+            this.UI_TotalAmountLabel.Size = new System.Drawing.Size(34, 13);
+            this.UI_TotalAmountLabel.TabIndex = 8;
+            this.UI_TotalAmountLabel.Text = "$0.00";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(380, 339);
-            this.Controls.Add(this.UI_TotalAmount);
-            this.Controls.Add(this.UI_GSTAmount);
-            this.Controls.Add(this.UI_SubtotalAmount);
+            this.Controls.Add(this.UI_TotalAmountLabel);
+            this.Controls.Add(this.UI_GSTAmountLabel);
+            this.Controls.Add(this.UI_SubtotalAmountLabel);
             this.Controls.Add(this.UI_CalculateButton);
             this.Controls.Add(this.UI_TotalLabel);
             this.Controls.Add(this.UI_GSTLabel);
             this.Controls.Add(this.UI_SubtotalLabel);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.UI_PurchaseBox);
+            this.Controls.Add(this.UI_CostBox);
             this.Controls.Add(this.UI_NumberLabel);
             this.Controls.Add(this.UI_CostLabel);
             this.Name = "Form1";
@@ -165,15 +169,15 @@
 
         private System.Windows.Forms.Label UI_CostLabel;
         private System.Windows.Forms.Label UI_NumberLabel;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox UI_CostBox;
+        private System.Windows.Forms.TextBox UI_PurchaseBox;
         private System.Windows.Forms.Label UI_SubtotalLabel;
         private System.Windows.Forms.Label UI_GSTLabel;
         private System.Windows.Forms.Label UI_TotalLabel;
         private System.Windows.Forms.Button UI_CalculateButton;
-        private System.Windows.Forms.Label UI_SubtotalAmount;
-        private System.Windows.Forms.Label UI_GSTAmount;
-        private System.Windows.Forms.Label UI_TotalAmount;
+        private System.Windows.Forms.Label UI_SubtotalAmountLabel;
+        private System.Windows.Forms.Label UI_GSTAmountLabel;
+        private System.Windows.Forms.Label UI_TotalAmountLabel;
     }
 }
 
--- ICA2_WilliamFichtner/Form1.cs
+++ ICA2_WilliamFichtner/Form1.cs
@@ -12,6 +12,8 @@ namespace ICA2_WilliamFichtner
 {
     public partial class Form1 : Form
     {
+        bool check1 = false;
+        bool check2 = false;
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +27,57 @@ namespace ICA2_WilliamFichtner
         private void label5_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void UI_CalculateButton_Click(object sender, EventArgs e)
+        {
+            double cost = 0.0;
+            double numberPurchased = 0.0;
+            double subtotal = 0.0;
+            double gstAmount = 0.0;
+            double total = 0.0;
+            const double gst = 0.05;
+
+            if ((double.TryParse(UI_CostBox.Text, out cost)) && (double.TryParse(UI_PurchaseBox.Text, out numberPurchased)))
+            {
+                subtotal = cost * numberPurchased;
+                UI_SubtotalAmountLabel.Text = subtotal.ToString("C");
+                gstAmount = subtotal * gst;
+                UI_GSTAmountLabel.Text = gstAmount.ToString("C");
+                total = subtotal + gstAmount;
+                UI_TotalAmountLabel.Text = total.ToString("C");
+            }
+            else
+            {
+                MessageBox.Show("You must enter valid amounts for each entry.", "ICA02", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            UI_CalculateButton.Enabled = false;
+            check1 = false;
+            check2 = false;
+        }
+
+        private void UI_CostBox_TextChanged(object sender, EventArgs e)
+        {
+            UI_SubtotalAmountLabel.Text = "$0.00";
+            UI_GSTAmountLabel.Text = "$0.00";
+            UI_TotalAmountLabel.Text = "$0.00";
+            check1 = true;
+            if ((check1 == true) && (check2 == true))
+            {
+                UI_CalculateButton.Enabled = true;
+            }
+        }
+
+        private void UI_PurchaseBox_TextChanged(object sender, EventArgs e)
+        {
+            UI_SubtotalAmountLabel.Text = "$0.00";
+            UI_GSTAmountLabel.Text = "$0.00";
+            UI_TotalAmountLabel.Text = "$0.00";
+            check2 = true;
+            if ((check1 == true) && (check2 == true))
+            {
+                UI_CalculateButton.Enabled = true;
+            }
         }
     }
 }
